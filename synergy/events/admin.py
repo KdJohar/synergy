0d800a38d872159212334e1e51afb742caaca6d8
Kd Johar <kdjohar91@gmail.com>
2015-10-04 13:00:58 +0530

    events models created

--- synergy/events/admin.py
+++ synergy/events/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import TableTennis, Badminton, Volleyball, Chess
 # Register your models here.
+
+class CommonCriteriaAdmin(admin.ModelAdmin):
+    # common for badminton and tabletennis
+    list_display = ('player_name', 'event_type', 'course', 'contact', 'email', )
+    list_filter = ('event_type', 'course', )
+    search_fields = ('player_name', 'second_player_name', 'contact', 'email', )
+
+class VolleyballAdmin(admin.ModelAdmin):
+    # common for badminton and tabletennis
+    list_display = ('captain', 'course', 'contact', 'email', )
+    list_filter = ( 'course', )
+    search_fields = ('captain', 'contact', 'email', )
+
+class ChessAdmin(admin.ModelAdmin):
+    # common for badminton and tabletennis
+    list_display = ('player_name', 'course', 'contact', 'email', )
+    list_filter = ( 'course', )
+    search_fields = ('player_name', 'contact', 'email', )
+
+admin.site.register(TableTennis, CommonCriteriaAdmin)
+admin.site.register(Badminton, CommonCriteriaAdmin)
+admin.site.register(Volleyball, VolleyballAdmin)
+admin.site.register(Chess, ChessAdmin)
--- synergy/events/models.py
+++ synergy/events/models.py
@@ -2,3 +2,82 @@ from django.db import models
 
 # Create your models here.
 
+
+class CommonCriteria(models.Model):
+
+    event_choices = (
+        ('s', 'single'),
+        ('d', 'double'),
+    )
+
+    course_choice = (
+        ('it', 'Mtech'),
+        ('ic','MCA'),
+        ('ib', 'Bcom(hons)'),
+        ('mb', 'MBA')
+    )
+    event_type = models.CharField(max_length=1, choices=event_choices)
+    player_name = models.CharField(max_length=100)
+    second_player_name = models.CharField(max_length=100, null=True, blank=True)
+    course = models.CharField(max_length=2, choices=course_choice)
+    email = models.EmailField()
+    contact = models.IntegerField()
+
+
+    class Meta:
+        abstract = True
+
+
+class TableTennis(CommonCriteria):
+
+    def __unicode__(self):
+
+        return self.player_name
+
+class Badminton(CommonCriteria):
+
+    def __unicode__(self):
+
+        return self.player_name
+
+class Volleyball(models.Model):
+
+    course_choice = (
+        ('it', 'Mtech'),
+        ('ic','MCA'),
+        ('ib', 'Bcom(hons)'),
+        ('mb', 'MBA')
+    )
+
+    course = models.CharField(max_length=2, choices=course_choice)
+    captain = models.CharField(max_length=100)
+    player1 = models.CharField(max_length=100)
+    player2 = models.CharField(max_length=100)
+    player3 = models.CharField(max_length=100)
+    player4 = models.CharField(max_length=100)
+    player5 = models.CharField(max_length=100)
+    extra1 = models.CharField(max_length=100)
+    extra2 = models.CharField(max_length=100)
+    contact = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True)
+
+
+    def __unicode__(self):
+        return self.captain
+
+class Chess(models.Model):
+
+    course_choice = (
+        ('it', 'Mtech'),
+        ('ic','MCA'),
+        ('ib', 'Bcom(hons)'),
+        ('mb', 'MBA')
+    )
+
+    course = models.CharField(max_length=2, choices=course_choice)
+    player_name = models.CharField(max_length=100)
+    contact = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True)
+
+    def __unicode__(self):
+        return self.player_name
